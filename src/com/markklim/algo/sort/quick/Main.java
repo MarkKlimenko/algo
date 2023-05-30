5cbc518e01b66fe886b60949453e9394175e5a00
markklimenko <m@k.com>
2023-05-30 23:38:25 +0400

    Add quick sort

--- src/com/markklim/algo/sort/quick/Main.java
+++ src/com/markklim/algo/sort/quick/Main.java
@@ -11,11 +11,46 @@ public class Main {
         int[] expectedArr = Arrays.copyOf(arr, arr.length);
         Arrays.sort(expectedArr);
 
-        sort(arr);
+        quickSort(arr);
         assertEquals(0, Arrays.compare(expectedArr, arr));
+
+        int[] arrEmpty = {};
+        int[] expectedEmpty = {};
+        quickSort(arrEmpty);
+        assertEquals(0, Arrays.compare(expectedEmpty, arrEmpty));
     }
 
-    private static void sort(int[] arr) {
+    private static void quickSort(int[] arr) {
+        sort(0, arr.length - 1, arr);
+    }
 
+    private static void sort(int start, int finish, int[] arr) {
+        if (start < finish) {
+            int pivotIndex = partition(start, finish, arr);
+            sort(start, pivotIndex - 1, arr);
+            sort(pivotIndex + 1, finish, arr);
+        }
+    }
+
+    private static int partition(int start, int finish, int[] arr) {
+        int pivot = arr[finish];
+        int i = start - 1;
+
+        for(int j = start; j < finish; j++) {
+            if(arr[j] < pivot) {
+                i++;
+                swap(i, j, arr);
+            }
+        }
+
+        i++;
+        swap(i, finish, arr);
+        return i;
+    }
+
+    private static void swap(int i, int j, int[] arr) {
+        int buf = arr[i];
+        arr[i] = arr[j];
+        arr[j] = buf;
     }
 }
